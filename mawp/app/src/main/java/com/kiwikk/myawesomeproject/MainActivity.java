60db2c6ea701218ca7797c6805a071c351112eb7
Elizabeth Frolova <edfrolova@edu.hse.ru>
2020-08-19 02:14:16 +0300

    androidx migration + bottomNavBar

--- mawp/app/src/main/java/com/kiwikk/myawesomeproject/MainActivity.java
+++ mawp/app/src/main/java/com/kiwikk/myawesomeproject/MainActivity.java
@@ -1,13 +1,49 @@
 package com.kiwikk.myawesomeproject;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
 import android.os.Bundle;
 
+import com.ismaeldivita.chipnavigation.ChipNavigationBar;
+
+
 public class MainActivity extends AppCompatActivity {
+
+    private ChipNavigationBar chipNavigationBar;
+    private Fragment fragment=null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        chipNavigationBar=findViewById(R.id.chipNavigation);
+        chipNavigationBar.setItemSelected(R.id.home, true);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, new HomeFragment()).commit();
+
+        chipNavigationBar.setOnItemSelectedListener(new ChipNavigationBar.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(int i) {
+                switch (i){
+                    case R.id.home:
+                    fragment = new HomeFragment();
+                    break;
+                    case R.id.news:
+                        fragment=new NewsFragment();
+                        break;
+                    case R.id.notifications:
+                        fragment=new NotificationsFragment();
+                        break;
+                    case R.id.settings:
+                        fragment=new SettingsFragment();
+                        break;
+                }
+
+                if(fragment != null) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+                }
+            }
+        });
     }
 }
